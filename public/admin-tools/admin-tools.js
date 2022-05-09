f30f9adc3a349a6c1a38c98213c7bd62440768ab
Nelite <neliitsme14@gmail.com>
2022-05-09 20:07:26 +0300

    feat: switch fetch to axios and add bootstrap toasts

--- public/admin-tools/admin-tools.js
+++ public/admin-tools/admin-tools.js
@@ -1,27 +1,87 @@
+function buildToast(toastConfig) {
+  let toastDiv = document.createElement('div');
+  let toastHeader = document.createElement('div');
+  let toastBody = document.createElement('div');
+
+  toastDiv.classList.add('toast');
+  toastDiv.classList.add('text-white');
+  if (toastConfig.type === 'success') {
+    toastDiv.classList.add('bg-success');
+  } else if (toastConfig.type === 'error') {
+    toastDiv.classList.add('bg-danger');
+  }
+  toastHeader.classList.add('toast-header');
+  toastBody.classList.add('toast-body');
+
+  toastDiv.setAttribute('role', 'alert');
+  toastDiv.setAttribute('aria-live', 'assertive');
+  toastDiv.setAttribute('aria-atomic', 'true');
+
+  toastHeader.append(document.createTextNode(toastConfig.status));
+  toastBody.append(document.createTextNode(toastConfig.message));
+
+  toastDiv.append(toastHeader, toastBody);
+
+  document.getElementById('toastContainer').append(toastDiv);
+
+  return new bootstrap.Toast(toastDiv);
+}
+
+function showToast(toastConfig) {
+  let toastEl = buildToast(toastConfig);
+  let toastElList = [].slice.call(document.querySelectorAll('.toast'));
+
+  if (toastElList.length > 7) {
+    toastElList.at(0).remove();
+  }
+
+  toastEl.show();
+  setTimeout(() => {
+    toastElList.at(-1).remove();
+  }, 5250);
+}
+
+function getSuccessResponseObject(response) {
+  return {
+    status: response.statusText,
+    message: JSON.stringify(response.data),
+    type: 'success',
+  };
+}
+
+function getErrorResponseObject(error) {
+  return {
+    status: error.response.statusText,
+    message: JSON.stringify(error.response.data),
+    type: 'error',
+  };
+}
+
+function tempoButton() {
+  showToast({ status: 'Tempo', message: 'Button', type: 'success' });
+}
+
 async function createItem() {
   const itemName = document.getElementById('postItemName').value;
   const itemDesc = document.getElementById('postItemDesc').value;
   const userId = document.getElementById('postItemUserId').value;
   const placeId = document.getElementById('postItemPlaceId').value;
 
-  let obj = {};
-  obj.name = itemName;
-  obj.description = itemDesc;
-  obj.userId = userId;
-  obj.placeId = placeId;
-
-  const stringObj = JSON.stringify(obj);
-  console.log(stringObj);
-
-  const response = await fetch('/items', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: stringObj,
-  }).then((data) => {
-    console.log(data.json());
-  });
+  axios
+    .post('/items', {
+      name: itemName,
+      description: itemDesc,
+      userId: userId,
+      placeId: placeId,
+    })
+    .then((response) => {
+      console.log(response);
+      showToast(getSuccessResponseObject(response));
+    })
+    .catch((error) => {
+      console.log(error);
+      showToast(getErrorResponseObject(error));
+    });
 
   document.getElementById('postItemName').value = '';
   document.getElementById('postItemDesc').value = '';
@@ -30,31 +90,35 @@ async function createItem() {
 }
 
 async function createPlace() {
-  const response = await fetch('/places', { method: 'POST' }).then((data) => {
-    console.log(data.json());
-  });
+  axios
+    .post('/places')
+    .then((response) => {
+      console.log(response);
+      showToast(getSuccessResponseObject(response));
+    })
+    .catch((error) => {
+      console.log(error);
+      showToast(getErrorResponseObject(error));
+    });
 }
 
 async function assignPlace() {
   const userId = document.getElementById('assignPlaceId').value;
   const placeId = document.getElementById('assignUserId').value;
 
-  let obj = {};
-  obj.userId = parseInt(userId);
-  obj.occupation = 'OCCUPIED'
-
-  const stringObj = JSON.stringify(obj);
-  console.log(stringObj);
-
-  const response = await fetch('/places/' + placeId, {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: stringObj,
-  }).then((data) => {
-    console.log(data.json());
-  });
+  axios
+    .patch('/places/' + placeId, {
+      userId: parseInt(userId),
+      occupation: 'OCCUPIED',
+    })
+    .then((response) => {
+      console.log(response);
+      showToast(getSuccessResponseObject(response));
+    })
+    .catch((error) => {
+      console.log(error);
+      showToast(getErrorResponseObject(error));
+    });
 
   document.getElementById('assignPlaceId').value = '';
   document.getElementById('assignUserId').value = '';
@@ -62,24 +126,51 @@ async function assignPlace() {
 
 async function deleteUser() {
   const userId = document.getElementById('deleteUserId').value;
-  const response = await fetch('/users/' + userId, { method: 'DELETE' }).then((data) => {
-    console.log(data.json());
-  });
-  document.getElementById('deleteUserId').value = "";
+
+  axios
+    .delete('/users/' + userId)
+    .then((response) => {
+      console.log(response);
+      showToast(getSuccessResponseObject(response));
+    })
+    .catch((error) => {
+      console.log(error);
+      showToast(getErrorResponseObject(error));
+    });
+
+  document.getElementById('deleteUserId').value = '';
 }
 
 async function deletePlace() {
   const placeId = document.getElementById('deletePlaceId').value;
-  const response = await fetch('/places/' + placeId, { method: 'DELETE' }).then((data) => {
-    console.log(data.json());
-  });
-  document.getElementById('deletePlaceId').value = "";
+
+  axios
+    .delete('/places/' + placeId)
+    .then((response) => {
+      console.log(response);
+      showToast(getSuccessResponseObject(response));
+    })
+    .catch((error) => {
+      console.log(error);
+      showToast(getErrorResponseObject(error));
+    });
+
+  document.getElementById('deletePlaceId').value = '';
 }
 
 async function deleteItem() {
   const itemId = document.getElementById('deleteItemId').value;
-  const response = await fetch('/items/' + itemId, { method: 'DELETE' }).then((data) => {
-    console.log(data.json());
-  });
-  document.getElementById('deleteItemId').value = "";
+
+  axios
+    .delete('/items/' + itemId)
+    .then((response) => {
+      console.log(response);
+      showToast(getSuccessResponseObject(response));
+    })
+    .catch((error) => {
+      console.log(error);
+      showToast(getErrorResponseObject(error));
+    });
+
+  document.getElementById('deleteItemId').value = '';
 }
