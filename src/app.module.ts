b6724a1e7aff4b54dbf0f9d5641b5fcd4e1641a5
Nelite <neliitsme14@gmail.com>
2022-05-13 20:12:09 +0300

    feat: add global console logger middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PlacesModule } from './places/places.module';
 import { ItemsModule } from './items/items.module';
+import LoggerMiddleware from './middleware/logger.middleware';
 
 @Module({
   imports: [ConfigModule.forRoot(), UsersModule, PlacesModule, ItemsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('*');
+  }
+}
